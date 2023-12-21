ea0158509041ac410f9fb804738a032a5a9a460c
Danial <mohammeddanial01@gmail.com>
2023-12-21 10:52:19 +0530

    Added new files

---
+++ webtech/js/script.js
@@ -0,0 +1,19 @@
+// console.log("HELLO");
+
+// // document.write("HelloWorld")
+// // document.write("HelloWorld")
+// // document.write("HelloSWorld")
+
+
+// // document.writeln("HelloWorld")
+// // document.writeln("HelloWorld")
+// // document.writeln("HelloWorld")
+
+// prompt("Enter your Greatgrandfather's name:")
+
+
+for(i=0;i<1;i++)
+{
+    console.log(i)    
+}
+
